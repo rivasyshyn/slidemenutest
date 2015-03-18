24a631dbc4e82ba6825a7754fceee8780e3c9e5d
Roman Ivasyshyn <shteyner@gmail.com>
2015-03-18 17:00:43 +0200

    test proj for slide menu problem

---
+++ SlideMenuTest/MainPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using SlideMenuTest.Resources;
+using System.Windows.Media.Animation;
+using System.Windows.Input;
+
+namespace SlideMenuTest
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button Clicked");
+        }
+
+        #region SlideMenu supporting code
+        private void OpenClose_Left(object sender, RoutedEventArgs e)
+        {
+            var left = Canvas.GetLeft(LayoutRoot);
+            if (left > -100)
+            {
+                ApplicationBar.IsVisible = true;
+                MoveViewWindow(-420);
+            }
+            else
+            {
+                ApplicationBar.IsVisible = false;
+                MoveViewWindow(0);
+            }
+        }
+
+        void MoveViewWindow(double left)
+        {
+            _viewMoved = true;
+            if (left == -420)
+            {
+                //ApplicationBar.IsVisible = true;
+                ContentContainer.IsHitTestVisible = true;
+            }
+            else
+            {
+                //ApplicationBar.IsVisible = false;
+                ContentContainer.IsHitTestVisible = false;
+            }
+
+            //((Storyboard)canvas.Resources["moveAnimation"]).SkipToFill();
+            ((DoubleAnimation)((Storyboard)canvas.Resources["moveAnimation"]).Children[0]).To = left;
+            ((Storyboard)canvas.Resources["moveAnimation"]).Begin();
+
+        }
+
+        private void canvas_ManipulationDelta(object sender, ManipulationDeltaEventArgs e)
+        {
+            if (e.DeltaManipulation.Translation.X != 0)
+            {
+                //ContentContainer.IsHitTestVisible = false; doesn't work !!!
+                Canvas.SetLeft(LayoutRoot, Math.Min(Math.Max(-420, Canvas.GetLeft(LayoutRoot) + e.DeltaManipulation.Translation.X), 0));
+            }
+        }
+
+        double initialPosition;
+        bool _viewMoved = false;
+        private void canvas_ManipulationStarted(object sender, ManipulationStartedEventArgs e)
+        {
+            _viewMoved = false;
+            initialPosition = Canvas.GetLeft(LayoutRoot);
+        }
+
+        private void canvas_ManipulationCompleted(object sender, ManipulationCompletedEventArgs e)
+        {
+            var left = Canvas.GetLeft(LayoutRoot);
+            if (_viewMoved)
+                return;
+            if (Math.Abs(initialPosition - left) < 100)
+            {
+                //bouncing back
+                MoveViewWindow(initialPosition);
+                return;
+            }
+            //change of state
+            if (initialPosition - left > 0)
+            {
+                //slide to the left
+                if (initialPosition > -420)
+                    MoveViewWindow(-420);
+                //else
+                //    MoveViewWindow(-840);
+            }
+            else
+            {
+                //slide to the right
+                if (initialPosition < -420)
+                    MoveViewWindow(-420);
+                else
+                    MoveViewWindow(0);
+            }
+
+        }
+
+        #endregion
+
+    }
+}
